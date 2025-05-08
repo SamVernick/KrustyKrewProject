9b32d315070118c5e7350a5a40eced040adc5d36
Bianca RiveraNales <riveranb@classwork.engr.oregonstate.edu>
2025-05-08 09:14:56 -0700

    Added update and delete for customers

--- frontend/src/pages/CustomersPage.jsx
+++ frontend/src/pages/CustomersPage.jsx
@@ -8,6 +8,34 @@ function CustomersPage() {
         <>
             <h2>List of Customers</h2>
             <CustomersTable/>
+
+            <h2>Update Customer</h2>
+            <form>
+            <label>Choose Customer to Update: </label>
+                <select>
+                    <option>Mermaid Man</option>
+                    <option>Barnacle Boy</option>
+                    <option>Patrick Star</option>
+                    <option>Bubble Bass</option>
+                </select>
+                <label>First Name: </label>
+                    <input type="text" />
+                <label>Last Name: </label>
+                    <input type="text" />
+                <input type="submit"/>
+            </form>
+
+            <h2>Delete Customer</h2>
+            <form>
+                <label>Choose a Customer to Delete: </label>
+                <select>
+                    <option>Mermaid Man</option>
+                    <option>Barnacle Boy</option>
+                    <option>Patrick Star</option>
+                    <option>Bubble Bass</option>
+                </select>
+                <input type="submit" />
+            </form>
         </>
     )
 }
