d1757871bccb089d4b54f613a48339227de48003
Bianca Rivera Nales <riveranb@classwork.engr.oregonstate.edu>
2025-05-22 17:13:41 -0700

    Made procedure for deleting a product

---
+++ backend/database/plsql.sql
@@ -0,0 +1,61 @@
+-- DROP PROCEDURE IF EXISTS sp_insert_customer;
+-- DELIMITER //
+
+-- CREATE PROCEDURE sp_insert_customer(
+--     IN c_fname varchar(45),
+--     IN c_lname varchar(45),
+--     IN c_money decimal(9,2),
+--     OUT new_customerID int
+-- )
+-- COMMENT "Adds a new customer to Customers table"
+-- BEGIN
+--     DECLARE c_id int;
+--     DECLARE EXIT HANDLER FOR SQLEXCEPTION
+--     BEGIN 
+--         ROLLBACK;
+--         SET new_customerID = -99;
+--     END;
+
+--     START TRANSACTION;
+
+--     INSERT INTO `Customers` (firstName, lastName, moneySpent)
+--     VALUES (c_fname, c_lname, c_money);
+
+--     SET c_id = LAST_INSERT_ID();
+--     SET new_customerID = c_id;
+--     COMMIT;
+
+-- END //
+-- DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS delete_product;
+DELIMITER //
+
+CREATE PROCEDURE delete_product(
+    IN product_id INT
+)
+COMMENT 'Deletes a product from the Product table'
+BEGIN 
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+
+    BEGIN 
+        ROLLBACK;
+        SELECT 'Deletion error' AS message;
+    END;
+
+    START TRANSACTION;
+
+    DELETE FROM Products WHERE id = product_id;
+    IF ROW_COUNT() = 0 THEN 
+        ROLLBACK;
+        SELECT 'Deletion error' AS message;
+    ELSE 
+        COMMIT;
+        SELECT 'Product deleted from Product table' AS message;
+    END IF;
+
+END //
+
+DELIMITER ;
