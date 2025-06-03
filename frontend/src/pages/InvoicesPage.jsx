0639759c3a78f25e89460af4441050376c3f7c43
Samuel Vernick <vernicks@classwork.engr.oregonstate.edu>
2025-06-02 22:18:05 -0700

    Partial Invoices

--- frontend/src/pages/InvoicesPage.jsx
+++ frontend/src/pages/InvoicesPage.jsx
@@ -1,7 +1,6 @@
 import InvoiceTable from "../components/InvoiceTable";
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import ResetButton from "../components/ResetButton";
 
 function InvoicesPage() {
     //const nvaigate = useNavigate();
@@ -10,7 +9,6 @@ function InvoicesPage() {
             <div className="bd-47">
                 <h1 className="text-xl font-medium underline underline-offset-2 mb-4 text-black">List of Invoices:</h1>
                 <InvoiceTable/>
-                <ResetButton></ResetButton>
             </div>
         </>
     )
